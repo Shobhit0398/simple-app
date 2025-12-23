21106c0d5b574963fea998c9a3eea99b8e01c324
shobhit . <shobhit.@shobhits-MacBook-Air.local>
2025-12-23 22:25:01 +0530

    new

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from CI/CD Pipeline!');
+});
+
+app.listen(3001, () => console.log('App running on port 3000'));
